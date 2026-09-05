25bd57243da7b7e6b7d17827f1c8a93c699fe3ce
RAM JI TRIVEDI <ramjitrivedi1801@gmail.com>
2026-09-05 18:50:48 +0530

    Add OR gate testbench in System Verilog
    This testbench simulates the behavior of an OR gate with various input combinations and outputs the results.

---
+++ 01_Basics/OR_Gate/or_gate_tb.sv
@@ -0,0 +1,39 @@
+// OR Gate Testbench - System Verilog
+
+module tb_or_sv;
+  logic a;
+  logic b;
+  logic y;
+  
+  or_sv dut(
+    .a(a),
+    .b(b),
+    .y(y)
+  );
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1,tb_or_sv);
+  
+  	$monitor("a=%b | b=%b | y=%b", a,b,y);
+  
+  	a=0;
+  	b=0;
+  	#10;
+  
+  	a=0;
+  	b=1;
+  	#10;
+  
+  	a=1;
+  	b=0;
+  	#10;
+  
+  	a=1;
+  	b=1;
+ 	#10;
+  
+    $display("a=%b | b=%b | y=%b",a,b,y);
+    $finish;
+  end
+endmodule

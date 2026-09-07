3e284ff319682c4cfaee52dd7fab8e6282a7bb00
RAM JI TRIVEDI <ramjitrivedi1801@gmail.com>
2026-09-07 19:16:29 +0530

    Create xnor_gate.sv

---
+++ 01_Basics/XNOR_Gate/xnor_gate.sv
@@ -0,0 +1,14 @@
+// XNOR Gate - System Verilog
+
+module xnor_gate(
+  input logic a,
+  input logic b,
+  output logic y
+);
+  
+  always_comb begin
+    
+    y = (~a)^b;   //y = (~a)*(~b) + (a*b);
+    
+  end
+endmodule

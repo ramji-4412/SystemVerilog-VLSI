56250584ea019e23c26badfa69fa2edf6304bdeb
RAM JI TRIVEDI <ramjitrivedi1801@gmail.com>
2026-09-10 19:08:15 +0530

    Add 4:2 encoder module in System Verilog

---
+++ 02_Combinational/4:2_ENCODER/encoder4to2.sv
@@ -0,0 +1,14 @@
+// 4:2 Encoder - System Verilog
+
+module encoder4to2(
+  input logic [3:0]D,
+  output logic [1:0]Y
+);
+  
+  always_comb begin
+    
+    Y[1] = D[3] | D[2];
+    Y[0] = D[3] | D[1];
+    
+  end
+endmodule

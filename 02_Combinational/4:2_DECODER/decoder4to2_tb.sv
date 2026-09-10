aaafaedda7e888bde2b95c12890386723f69061a
RAM JI TRIVEDI <ramjitrivedi1801@gmail.com>
2026-09-10 18:54:40 +0530

    Add testbench for 4:2 decoder in System Verilog

---
+++ 02_Combinational/4:2_DECODER/decoder4to2_tb.sv
@@ -0,0 +1,30 @@
+// 4:2 Decoder Testbench - System Verilog
+
+module decoder4to2_tb;
+  logic [3:0]D;
+  logic [1:0]Y;
+  
+  decoder4to2 dut(
+    .D(D),
+    .Y(Y)
+  );
+  
+  initial begin
+    
+    $dumpfile("dump.vcd");
+    $dumpvars(1,decoder4to2_tb);
+    
+    $monitor("D=%b | Y=%b",D,Y);
+    
+    D=4'b0001;
+    #10;
+    D=4'b0010;
+    #10;
+    D=4'b0100;
+    #10;
+    D=4'b1000;
+    #10
+  
+    $finish;
+  end
+endmodule

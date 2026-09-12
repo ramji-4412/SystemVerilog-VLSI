86666df5ac2ec76f1b8fdcd8041d3c8c1d9a83c2
RAM JI TRIVEDI <ramjitrivedi1801@gmail.com>
2026-09-12 19:38:10 +0530

    Create comparator_4bit_tb.sv

---
+++ 02_Combinational/4Bit_Comparator/comparator_4bit_tb.sv
@@ -0,0 +1,33 @@
+// Comparator 4bit Testbench - System Verilog
+
+module comparator_4bit_tb;
+  logic [3:0]A;
+  logic [3:0]B;
+  logic Y1,Y2,Y3;
+  
+  comparator_4bit dut(
+    .A(A),
+    .B(B),
+    .Y1(Y1),
+    .Y2(Y2),
+    .Y3(Y3)
+  );
+  
+  initial begin
+    
+    $dumpfile("dump.vcd");
+    $dumpvars(1,comparator_4bit_tb);
+    
+    $display("Y1 => A greater than B");
+    $display("Y2 => B greater than A");
+    $display("Y3 => A equals B");
+    $monitor("A=%b | B=%b | Y1=%b | Y2=%b | Y3=%b",A,B,Y1,Y2,Y3);
+    
+    for(int i=0;i<256;i++)begin
+      {A,B} = i;
+      #10;
+    end
+    
+    $finish;
+  end
+endmodule
